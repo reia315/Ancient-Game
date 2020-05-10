40f090d7a1318d33806115765125358bf77b7409
MSI\g019g1418 <star15yuji@gmail.com>
2020-05-10 22:28:47 +0900

    Add Shpere.h

---
+++ Ancient game/Ancient game/Shpere.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <Windows.h>
+#include <DirectXMath.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+
+using namespace DirectX;
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+class Shpere
+{
+public:
+
+	Shpere();
+
+	~Shpere();
+
+	void ShpereMake();
+
+	void ShpereMaker();
+
+};
