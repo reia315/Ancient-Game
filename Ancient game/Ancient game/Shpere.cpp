c79f30e8aae5691fb681bdd84d1cd518301f3ecb
MSI\g019g1418 <star15yuji@gmail.com>
2020-05-14 00:53:49 +0900

    Update Shpere.cpp

--- Ancient game/Ancient game/Shpere.cpp
+++ Ancient game/Ancient game/Shpere.cpp
@@ -1,59 +1,36 @@
 #include "Shpere.h"
 
-
-Shpere::Shpere()
+//コンストラクタ
+Shpere::Shpere(ID3D11Device* device, ID3D11DeviceContext* deviceContext)
+	:m_pDevice{ device }, m_pDeviceContext{deviceContext}
 {
+	// シェーダの設定
+	ID3DBlob *pCompileVS = NULL;
+	D3DCompileFromFile(L"shader.hlsl", NULL, NULL, "VS", "vs_5_0", NULL, 0, &pCompileVS, NULL);
+	m_pDevice->CreateVertexShader(pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), NULL, &m_pVertexShader);
 
-}
+	ID3DBlob *pCompilePS = NULL;
+	D3DCompileFromFile(L"shader.hlsl", NULL, NULL, "PS", "ps_5_0", NULL, 0, &pCompilePS, NULL);
+	m_pDevice->CreatePixelShader(pCompilePS->GetBufferPointer(), pCompilePS->GetBufferSize(), NULL, &m_pPixelShader);
 
-Shpere::~Shpere()
-{
-}
-
-void Shpere::ShpereMaker(ID3D11Device *pDevice, ID3D11Buffer *pVertexBuffer, ID3D11Buffer *pIndexBuffer)
-{
-
-	//----------------------------------------------------
-	 //一つの頂点情報を格納する構造体
-	struct VERTEX {
-		XMVECTOR V;
+	// 頂点レイアウト
+		//構造体VERTEXの内部構成（ここではメンバVの正体が何なのかをDirectXに知らせるための物です）
+	D3D11_INPUT_ELEMENT_DESC layout[] = {
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 	};
+	m_pDevice->CreateInputLayout(layout, 1, pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), &m_pVertexLayout);
+	pCompileVS->Release();
+	pCompilePS->Release();
 
-
-	// GPU(シェーダ側)へ送る数値をまとめた構造体
-	struct CONSTANT_BUFFER {
-		XMMATRIX mWVP;
-	};
-	//------------------------------------------------------
-
-	//--------------------------------------------------------------------
-//球体に関するデータを保持する変数の宣言
-	const int u_max = 30;//以下２行球体の縦横の分割数を指定する定数
-	const int v_max = 15;
-	VERTEX *vertices;    //球体の頂点データを保持する変数
-	int *indexes;        //球体のインデックスデータを保持する変数
-	int vertex_num = u_max * (v_max + 1);//頂点数の計算
-	int index_num = 2 * v_max * (u_max + 1);//インデックス数の計算
-	//---------------------------------------------------------------------
-
-
-	
-
-
-		// 頂点データの作成
-	vertices = new VERTEX[vertex_num];
-	for (int v = 0; v <= v_max; v++) {
-		for (int u = 0; u < u_max; u++) {
-			double theta = XMConvertToRadians(180.0f * v / v_max);
-			double phi = XMConvertToRadians(360.0f * u / u_max);
-			double x = sin(theta) * cos(phi);
-			double y = cos(theta);
-			double z = sin(theta) * sin(phi);
-			vertices[u_max * v + u].V = XMVectorSet(x, y, z, 1.0f);
-		}
-
-	}
-
+	// 定数バッファの設定(パラメータ受け渡し用)
+	D3D11_BUFFER_DESC cb;
+	cb.ByteWidth = sizeof(CONSTANT_BUFFER);
+	cb.Usage = D3D11_USAGE_DYNAMIC;
+	cb.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cb.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cb.MiscFlags = 0;
+	cb.StructureByteStride = 0;
+	m_pDevice->CreateBuffer(&cb, NULL, &m_pConstantBuffer);
 
 	// 頂点データ用バッファの設定
 	D3D11_BUFFER_DESC bd_vertex;
@@ -65,27 +42,10 @@ void Shpere::ShpereMaker(ID3D11Device *pDevice, ID3D11Buffer *pVertexBuffer, ID3
 	bd_vertex.StructureByteStride = 0;
 	D3D11_SUBRESOURCE_DATA InitData_vertex;
 	InitData_vertex.pSysMem = vertices;
-	pDevice->CreateBuffer(&bd_vertex, &InitData_vertex, &pVertexBuffer);
-
-	// 球体のインデックスデータの作成
-	int i = 0;
-	indexes = new int[index_num];
-	for (int v = 0; v < v_max; v++) {
-		for (int u = 0; u <= u_max; u++) {
-			if (u == u_max) {
-				indexes[i++] = v * u_max;
-				indexes[i++] = (v + 1) * u_max;
-			}
-			else {
-				indexes[i++] = (v * u_max) + u;
-				indexes[i++] = indexes[i - 1] + u_max;
-			}
-		}
-	}
-
+	m_pDevice->CreateBuffer(&bd_vertex, &InitData_vertex, &m_pVertexBuffer);
 	
 	// インデックスデータ用バッファの設定
-	D3D11_BUFFER_DESC bd_index;
+    D3D11_BUFFER_DESC bd_index;
 	bd_index.ByteWidth = sizeof(int) * index_num;
 	bd_index.Usage = D3D11_USAGE_DEFAULT;
 	bd_index.BindFlags = D3D11_BIND_INDEX_BUFFER;
@@ -94,8 +54,40 @@ void Shpere::ShpereMaker(ID3D11Device *pDevice, ID3D11Buffer *pVertexBuffer, ID3
 	bd_index.StructureByteStride = 0;
 	D3D11_SUBRESOURCE_DATA InitData_index;
 	InitData_index.pSysMem = indexes;
-	pDevice->CreateBuffer(&bd_index, &InitData_index, &pIndexBuffer);
+	m_pDevice->CreateBuffer(&bd_index, &InitData_index, &m_pIndexBuffer);
 
+	// ラスタライザの設定
+	D3D11_RASTERIZER_DESC rdc = {};
+	rdc.FillMode = D3D11_FILL_WIREFRAME;
+	rdc.CullMode = D3D11_CULL_NONE;
+	rdc.FrontCounterClockwise = TRUE;
+	m_pDevice->CreateRasterizerState(&rdc, &m_pRasterizerState);
+
+	
+}
+
+Shpere::~Shpere()
+{
+	m_pSwapChain->Release();
+	m_pDeviceContext->Release();
+	m_pDevice->Release();
+
+	m_pBackBuffer_RTV->Release();
+
+	m_pRasterizerState->Release();
+	m_pVertexShader->Release();
+	m_pVertexLayout->Release();
+	m_pPixelShader->Release();
+	m_pConstantBuffer->Release();
+	m_pVertexBuffer->Release();
+	m_pIndexBuffer->Release();
+
+	delete[] vertices;
+	delete[] indexes;
+}
+
+void Shpere::ShpereMaker()
+{
 
 
 
